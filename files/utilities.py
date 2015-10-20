c13e462aa436ca0b49606b2cb6ee3130f2ff7b0b
Kari A. Frank <franka01@gmail.com>
2015-10-20 12:47:43 -0400

    Added files/ subpackage.

---
+++ files/utilities.py
@@ -0,0 +1,257 @@
+#----------------------------------------------------------
+#Module of miscellaneous useful functions involving files
+#
+#Contains the following functions:
+#
+# ls_to_list
+# fetch_file
+# read_list
+# file_lines
+# parse_file_line
+#----------------------------------------------------------
+
+#-import common modules-
+import os,re,sys,commands
+
+
+#----------------------------------------------------------
+#ls_to_list()
+
+#Author: Kari Frank
+#Date: January 22, 2013
+#Purpose: read in a path and return list of all files in that 
+#         directory as list of strings
+#Usage: ls_to_list('search_dir')
+#Input: 
+#  search_dir: string containing the full path to desired directory
+#  ls_args: optional string containing arguments to the ls command
+#Output:
+#  returns a list of strings, one element per file
+#Usage Notes:
+# 
+# 
+
+def ls_to_list(search_dir,ls_args=''):
+
+  #-set initial directory-
+  pwd = os.getcwd()
+    
+  #-temporarily change to desired directory-
+  os.chdir(search_dir)
+
+  #-get list of files-
+  longstr = commands.getoutput("ls "+ls_args) #stores as single string
+  
+  #-parse into separate files-
+  file_list = longstr.split('\n')
+
+  #-return to original working directory-
+  os.chdir(pwd)
+
+  return file_list
+
+
+#----------------------------------------------------------
+#fetch_file()
+
+#Author: Kari Frank
+#Date: January 22, 2013
+#Purpose: get a single file from given directory that 
+#         matches the search string pattern. if multiple 
+#         matching files or none, can prompt user for
+#         file path.
+#Usage: fetch_file('search_dir',pat)
+#Input: 
+#  search_dir: string containing the full path to directory
+#  pat: regular expression string containing the desired
+#        pattern to match (e.g. 'evt2'). default='evt2'
+#  prompt: Only matters if multiple matching files are found.
+#          if set to 'no' then will not prompt user for file
+#          in event of multiple matching files, will instead
+#          just pick the first one and print out a warning.
+#          can also be set to a (string)number, in which case it 
+#          will automatically choose that numbered file from the 
+#          available choices (starts with 0), as if the user
+#          had typed a number with prompt='yes'. 
+#          default is prompt='yes'. 
+#
+#Output:
+#  string containing full path to a single file
+#
+#Usage Notes:
+# - If there is no file that matches, will result in an error.
+#
+def fetch_file(search_dir,pat='evt2',prompt='yes'):
+
+  #-create list of all files in the directory
+  # which match the given pattern- 
+  file_list = ls_to_list(search_dir)
+  good_files = []
+  for f in file_list:
+    match = re.search(pat,f)
+    if match: #add all matching files to the good file list
+      good_files.append(search_dir+'/'+f)
+
+  #-decide on just one file-
+  if len(good_files) == 1: #if only one match found, return the path
+    return good_files[0] 
+  elif len(good_files) > 1: #if multiple matching files
+    if prompt == 'no':
+      print 'Warning: Multiple files found matching '+pat+' ;'
+      print '         choosing '+good_files[0]
+      return good_files[0]
+    else:
+      if prompt == 'yes': #if prompt was not set to a number
+        print 'More than one file found matching '+pat+','
+        print 'please choose from the following:'
+        for g in range(len(good_files)):
+          print good_files[g]+' ['+str(g)+']'
+        ind = input('Number of file: ')
+      else: #if prompt set to a number
+        ind = int(prompt)
+      return good_files[ind]
+  else: #if no matching files found
+    if prompt=='no':
+      sys.exit('ERROR: no file found in '+search_dir+' matching '+pat)
+    else:
+      file = raw_input('No file found matching '+pat+'\nEnter full path 
+                       to file: ')
+      if os.path.isfile(file)==False: #make sure given file exists
+        sys.exit('ERROR: Specified file does not exist.')
+      else:
+        return file
+#----------------------------------------------------------
+
+#----------------------------------------------------------
+#read_list()
+
+#Author: Kari Frank
+#Date: July 30, 2013
+#Purpose: return a list containing one element for each line
+#         of the specified file
+#
+#Usage: list = read_list(listfile,comment=comment)
+#
+#Input: 
+#  listfile: string containing the path and name of the file
+#            to be read  
+#
+#  comment: optional string to define as comment character.
+#           lines beginning with comment will be ignored.
+#
+#Output:
+#  - Returns a list of the file contents (one element per line).
+#    New line characters are stripped from the end of lines.
+#
+#Usage Notes:
+#
+#
+def read_list(listfile,comment=''):
+
+  #-open file-
+  thefile = open(listfile)
+ 
+  #-read in lines-
+  lines = thefile.readlines()
+  
+  #-strip newlines-
+  trimmed_lines = [line.strip() for line in lines]
+
+  #-remove commented lines-
+  if comment !='':
+    good_lines = []
+    for line in trimmed_lines:
+      if (line[0] != comment) and (line != ''): good_lines.append(line)
+  else:
+    good_lines = trimmed_lines
+
+  #-close file-
+  thefile.close()
+
+  #-return list-
+  return good_lines
+
+#----------------------------------------------------------
+#file_lines()
+
+#Author: Kari Frank
+#Date: March 20, 2014
+#Purpose: return the number of non-commented lines in a file
+#
+#Usage: nlines = file_lines(infile,comment=comment)
+#
+#Input: 
+#  infile: string containing the path and name of the file
+#            to be read  
+#
+#  comment: optional string to define as comment character.
+#           lines beginning with comment will be ignored.
+#
+#Output:
+#  - Returns number of non-commented lines in file 
+#
+#Usage Notes:
+#
+#
+def file_lines(infile,comment=''):
+
+  #--get list of lines--
+#  lines = read_lines(infile,comment=comment)
+
+  counter = 0
+  with open(infile,'r') as f:
+    for line in f:
+      counter += 1
+  
+  #--size of list--
+  return counter
+
+#----------------------------------------------------------
+# parse_file_line()
+#
+#Author: Kari A. Frank 
+#Date: March 20, 2014
+#Purpose: Read a first line from a file and parse it into a string list.
+#          
+#
+#Usage: parnames = parse_file_line(infile,delimiter=delimiter)
+#
+#Input:
+#
+#  infile    -- Path to text file.
+#  delimiter -- Optional delimiter(s) (string).  Default is space,
+#                comma, tab, and semicolon.  Can provide more than
+#                one delimiter character in the string.  Spaces and 
+#                tabs should be given in regular expression syntax,
+#                \s and \t.
+#
+#Output:
+#  
+#  returns string list of 'words' in the line.
+#
+#Usage Notes:
+#
+
+def parse_file_line(infile,delimiter=',;\s\t'):
+
+  #--set delimiter string--
+  delstr = r'[^'+delimiter+']+'
+
+  #--open file--
+  thefile = open(infile)
+
+  #--read line--
+  rawline = thefile.readlines()
+
+  #--close file--
+  thefile.close()
+
+  #--strip newline--
+  line = rawline[0].strip()
+
+  #--parse line--
+#    outpars = line.split()
+  outlist = re.findall(delstr,line)
+
+  #--return string list--
+  return outlist
---
+++ files/xmcrun.py
@@ -0,0 +1,225 @@
+#----------------------------------------------------------
+#Module of functions for dealing with files output from xmc
+#
+#Contains the following functions:
+#
+# parse_file_line
+# read_parnames
+# get_xmcoutput
+
+# functions using pandas:
+#  merge_output -- replaces get_deconvolution, get_statistic, and get_xmcoutput
+#
+#----------------------------------------------------------
+#Import common modules
+import os
+import numpy as np
+import pandas as pd
+import utilities
+
+#----------------------------------------------------------
+# read_parnames()
+#
+#Author: Kari A. Frank 
+#Date: March 20, 2014
+#Purpose: Read in parameters.txt file from an xmc run to get 
+#          names of columns in deconvolution files.
+#
+#Usage: parnames = read_parnames(runpath)
+#
+#Input:
+#
+#  runpath -- Path to xmc run directory, containing the file parameters.txt.
+#             Only the first line of the file is read.  Parameter names
+#             should contain no spaces or punctuation and are assumed to be 
+#             separated by commas.
+#
+#Output:
+#  
+#  returns string list of column (parameter) names
+#
+#Usage Notes:
+#
+
+def read_parnames(runpath):
+
+    #--Set file paths--
+    parfile = runpath+'/parameters.txt'
+
+    outpars = utilities.parse_file_line(parfile)
+
+    #--return string list--
+    return outpars
+
+#----------------------------------------------------------
+# merge_output
+#
+#Author: Kari A. Frank 
+#Date: October 16, 2015
+#Purpose: Read in deconvolution, statistic, or other files from an xmc run
+#          and merge into a single tab-separated text file, with an extra
+#          column to specify iteration
+#          
+#Usage: merge_output(filepath,filetype='deconvolution')
+#
+#Input:
+#
+#  filepath     -- Path to xmc run directory, containing the
+#                  deconvolution, statistic, etc. files
+# 
+#  filetype     -- string containing the type of xmc file to merge
+#                  deconvolution, statistic, sigma, mean, changed
+#
+#Output:
+#  writes a text file <filetype>_merged.txt into the filepath directory
+#  returns the new file name as a string
+#
+#Usage Notes:
+#  - overwrites the file if it already exists
+#  - automatically includes all iterations present
+#  - iterations are not written in order
+#  - the created file can be read into a dataframe using
+#    datatable = pd.read_table(mergedfile,sep='\t')
+#
+
+def merge_output(filepath,filetype='deconvolution'):
+
+    # - get list of files - 
+    filelist = utilities.ls_to_list(filepath,ls_args = filetype+'*')
+    
+    # --Initialize dataframe with first file --
+
+    # - read file - 
+    datatable = pd.read_table(filepath+'/'+filelist[0],sep='\s+',header=None)
+
+    # - add headers -
+    parnames = []
+    # read header information
+    if filetype == 'statistic':
+        parnames =  ['stat1','dof','oversim','nblobs','alpha','chi2','iteration']
+        datatable.columns = parnames
+    if filetype == 'deconvolution':
+        if os.path.isfile(filepath+'/parameters.txt'):
+            parnames = utilities.parse_file_line(filepath+'/parameters.txt') 
+            datatable.columns = parnames
+
+    # - get iteration number and add as column - 
+    iternum = int(filelist[0].split('.')[-1]) # returns integer
+    datatable['iteration'] = iternum
+
+    # -- Loop through files and concatenate into single dataframe --
+    for f in filelist[1:]:
+        print f
+        newframe = pd.read_table(filepath+'/'+f,sep='\s+',header=None)
+        iternum = int(f.split('.')[-1]) # returns integer
+        if len(parnames) > 0: newframe.columns=parnames
+        newframe['iteration'] = iternum
+        datatable = pd.concat([datatable,newframe],ignore_index=True)
+        
+    # -- Write to file --
+    datatable.to_csv(filepath+'/'+filetype+'_merged.txt',sep='\t')
+
+    return filetype+'_merged.txt'
+
+#----------------------------------------------------------
+# get_xmcoutput
+#
+#Author: Kari A. Frank 
+#Date: March 24, 2014
+#Purpose: Read in deconvolution or statistic files from an xmc run
+#          and return as a large array, with one parameter
+#          per column and an extra column for iteration number
+#          
+#
+#Usage: deconvolution_array = get_xmcoutput(runpath,itmin=itmin,itmax=itmax,
+#                                           kind='deconvolution')
+#
+#Input:
+#
+#  runpath     -- Path to xmc run directory, containing the
+#                  deconvolution files.
+# 
+#  itmin,itmax -- minimum and maximum number of iterations to read
+#                 default is 0 to maximum available
+#
+#  kind        -- optional string to specify which type of output
+#                 file should be read, 'deconvolution' (default)
+#                 of 'statistic'.
+#
+#Output:
+#  
+#  returns a tuple of the float array of deconvolution (or statistic)
+#     file content for all iterations in range itmin-itmax, and a 
+#     string list of the parameter(column) names: (array,parnames)
+#
+#Usage Notes:
+#
+
+def get_xmcoutput(runpath,itmin=0,itmax=-1,kind='deconvolution'):
+
+    if itmax == -1 :
+        itmax = len(utilities.ls_to_list(runpath,'deconvolution.*'))-1
+
+    #--set base filenames--
+    dfile = runpath+'/'+kind+'.'
+    parfile = runpath+'/parameters.txt'
+
+    #--get parameter (column) names--
+    if kind == 'deconvolution':
+        parnames = read_parnames(runpath)
+    else:
+        parnames = ['stat1','dof','oversim','nblobs','alpha','chi2']
+    npars = len(parnames)
+
+    #--blob (row) numbers--
+    if kind == 'deconvolution':
+        nblobs = utilities.file_lines(dfile+'0')
+#        blobnums = range(0,nblobs-1) #blob numbers start at 0
+    else:
+        nblobs = 1 #only one line in statistic files
+
+    #--loop through deconvolution files--
+    
+    for i in range(itmin,itmax):
+        
+        ifile = dfile+str(i)
+#        print ifile
+        
+        #-define new empty array-
+        total_arr = np.empty((nblobs,npars+1))
+
+        #-read file content into numpy array-
+        # first dimension is blobnumber (row), second
+        # dimension is parameter (column)
+
+        ## eventually turn this into a separate function
+        ## which takes as arguments the parnames (or can 
+        ## optionally read them from a header line in the
+        ## file itself) and a single file name, so that it
+        ## can also read in files written by write_blobs()
+
+        #-check if file exists and is not empty-
+        if (path.isfile(ifile) == False) or (path.getsize(ifile) == 0):
+            print "WARNING: "+ifile+" is missing or empty and will be skipped."
+        else:
+            total_arr[:,:-1] = np.loadtxt(ifile)
+
+            #-set last column to iteration number-
+            total_arr[:,-1] = i
+
+            #-concatenate onto master array-
+            if i == itmin:
+                master_array = total_arr
+            else:
+                master_array = np.vstack((master_array,total_arr))
+
+    #--append iteration to parnames--
+    parnames.append('iteration')
+
+    #--convert parnames list to an array--
+#    parnames_array = numpy.array(parnames)
+
+    #--return array and parameter names--
+    return (master_array,parnames)
+
+#----------------------------------------------------------
