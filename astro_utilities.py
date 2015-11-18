ce1b6bbeba0e69ada73c2c6241be92530b9957fd
Kari Frank <kaf33@E052497.astro.psu.edu>
2015-11-18 14:50:15 -0500

    Added coordinate conversion functions to astro_utilities

--- astro_utilities.py
+++ astro_utilities.py
@@ -1,17 +1,21 @@
-#----------------------------------------------------------
-#Module of handy general astronomy functions
-#
-#Contains the following functions:
-#
-# norm_to_em
-# transfer_header()
-# normalize_image()
-# stack_images()
-# angstroms2keV()
-# K2keV()
-# convert_distance()
-#
-#----------------------------------------------------------
+"""
+Module of handy general astronomy functions
+
+Contains the following functions:
+
+ norm_to_em
+ transfer_header()
+ normalize_image()
+ stack_images()
+ angstroms2keV()
+ K2keV()
+ convert_distance()
+ get_xmm_attitude()
+ hms2deg()
+ deg2hms()
+ xmc_wcs_convert()
+
+"""
 #Import common modules
 import shutil
 import numpy as np
@@ -326,4 +330,222 @@ def convert_distance(val,fromunit,tounit):
         if tounit == 'ly':
             return val*kpc_to_km*km_to_ly
 
+#----------------------------------------------------------
+def get_xmm_attitude(attfile='atthk.fits',hms=False):
+  """
+  get_xmm_attitude
+
+  Author: Kari Frank
+  Date: November 17, 2015
+  Purpose: Return an XMM observation boresight coordinates and rotation 
+           angle from an attitude (e.g. atthk.fits) file.
+
+  Usage: obscoords = get_xmm_attitude(attfile='atthk.fits',hms=FALSE)
+  Input:
+      attfile : [string] filename (including path) of the attitude file
+
+      hms : [boolean] return the coordinates as [hour,minutes,seconds] 
+            instead of degrees
+
+  Output:
+      Returns list of coordinates and angle, 
+      [right_ascension,declination,angle], where all are in decimal degrees
+
+      If hms=True, then right_ascension and declination are lists of the 
+      form [hh,mm,ss]
+
+  Usage Notes:
+     - the attitude file should be generated from the odf files using 
+       the XMM-SAS tool atthkgen
+  """
+  
+  #-ra, dec, and rotation from attitude file header-
+  att_hdus = fits.open(attfile)
+  att_hdr = att_hdus[0].header
+  # this retrieves the median values in case they were not constant, e.g. 
+  # due to a slew failure
+  att_ra = att_hdr['MAHFRA']
+  att_dec = att_hdr['MAHFDEC']
+  att_ang = att_hdr['MAHFPA']
+  att_hdus.close()
+
+  #-convert to hh,mm,ss if hms=True-
+  if hms:
+    coords = [deg2hms(att_ra),deg2hms(att_dec),att_ang]
+  else:
+    coords = [att_ra,att_dec,att_ang]
+
+  #-return coordinates and angle-
+  return coords
+
+#----------------------------------------------------------
+def deg2hms(x_deg):
+  """
+  Author: Kari Frank
+  Date: November 17, 2015
+  Purpose: Convert a value from decimal degrees into hours,minutes,seconds.
+
+  Usage: y = deg2hms(x_deg)
+
+  Input:
+      x_deg : value in decimal degrees (e.g. right ascension)
+
+  Output:
+      Returns the converted value as a list in the form [hh,mm,ss]
+
+  Usage Notes:
+  """
+  
+  #-set constants-
+  day_length = 24.0
+
+  #-convert to hours-
+  x_hour_full = x_deg*day_length/360.0
+  x_hour = np.floor(x_hour_full)
+  #-convert remainder to minutes
+  x_min_full = (x_hour_full - x_hour)*60.0
+  x_min = np.floor(x_min)
+  #-convert remainder to seconds-
+  x_sec = (x_min_full - x_min)*60.0
+
+  return [x_hour,x_min,x_sec]
+#----------------------------------------------------------
+def hms2deg(x_hms):
+  """
+  Author: Kari Frank
+  Date: November 17, 2015
+  Purpose: Convert a value from hours,minutes,seconds into decimal degrees.
+           (This is the inverse of deg2hms)
+
+  Usage: y = hms2deg(x_hms)
+
+  Input:
+      x_hms : list in format [hh,mm,ss] (e.g. right ascension)
+
+  Output:
+      Returns the input as a single value in decimal degrees
+
+  Usage Notes:
+  """
+  
+  #-set constant-
+  day_length = 24.0
+
+  #-convert hours to degrees-
+  x_deg = x_hms[0]*360.0/day_length
+  #-convert minutes to degrees-
+  x_deg = x_deg + x_hms[1]*360.0/(60.0*day_length)
+  #-convert seconds to degrees-
+  x_deg = x_deg + x_hms[2]*360.0/(60.0*60.0*day_length)
+
+  return x_deg
+#----------------------------------------------------------
+def xmc2wcs(phi,psi,attfile='atthk.fits'):
+  """
+  Author: Kari Frank
+  Date: November 18, 2015
+  Purpose: Convert from xmc coordinates to RA and DEC.
+
+  Usage: y = xmc2wcs(phi,psi,attfile='atthk.fits')
+
+  Input:
+     phi (float): xmc phi (x) coordinate (arcsec)
+     psi (float): xmc psi (y) coordinate (arcsec)
+     attfile (string) : path to the observation's attitude file, 
+                        as created by the XMM-SAS tool atthkgen
+
+  Output:
+     Returns list [ra,dec] of the input xmc coordinates in decimal degrees.
+
+  Usage Notes:
+    - See also wcs2xmc()
+  """
+  
+  #--Set Center Coordinates--
+
+  #-get observation boresight and rotation angle-
+  obscoords = get_xmm_attitude(attfile=attfile) #=[ra,dec,angle]
+  obs_ra = obscoords[0]
+  obs_dec = obscoords[1]
+  obs_ang = obscoords[2]*np.pi/180.0 - np.pi/2.0
+
+  #-set xmc center coordinates-
+  phi0 = -60.0
+  psi0 = 80.0
+
+  #-convert xmc center coords to ra and dec in degrees-
+  ra0 = (obs_ra + (1.0/np.cos(obs_dec*np.pi/180.))*
+         (-1.0*phi0*np.cos(obs_ang)-psi0*np.sin(obs_ang))/3600.)
+  dec0 = obs_dec - (-1.0*phi0*np.sin(obs_ang)+psi0*np.cos(obs_ang))/3600.
+
+  #--Convert to WCS--
+  
+  #-shift phi and psi-
+  psi = -(psi-psi0)
+  phi = phi - phi0
+
+  #-rotate phi and psi-
+  phi_rot = phi*np.cos(obs_ang) - psi*np.sin(obs_ang)
+  psi_rot = phi*np.sin(obs_ang) - psi*np.cos(obs_ang)
+
+  #-shift to ra and dec and convert units-
+  ra = ra0 - (1.0/np.cos(dec0*np.pi/180.0))*phi_rot/3600.0
+  dec = dec0 + psi_rot/3600.
+
+  return [ra,dec]
+#----------------------------------------------------------
+def wcs2xmc(ra,dec,attfile='atthk.fits'):
+  """
+  Author: Kari Frank
+  Date: November 18, 2015
+  Purpose: Convert from RA and DEC into phi and psi coordinates used by xmc.
+
+  Usage: y = wcs2xmc(ra,dec,attfile='atthk.fits')
+
+  Input:
+     ra (float): right ascension in decimal degrees
+     dec (float): declination in decimal degrees
+     attfile (string) : path to the observation's attitude file, 
+                        as created by the XMM-SAS tool atthkgen
+
+  Output:
+     Returns list [phi,psi] of the input ra and dec coordinates 
+     in arcsec.
+
+  Usage Notes:
+     - See also xmc2wcs()
+  """
+  
+  #--Set Center Coordinates--
+
+  #-get observation boresight and rotation angle-
+  obscoords = get_xmm_attitude(attfile=attfile) #=[ra,dec,angle]
+  obs_ra = obscoords[0]
+  obs_dec = obscoords[1]
+  obs_ang = obscoords[2]*np.pi/180.0 - np.pi/2.0
+
+  #-set xmc center coordinates-
+  phi0 = -60.0
+  psi0 = 80.0
+
+  #-convert xmc center coords to ra and dec in degrees-
+  ra0 = (obs_ra + (1.0/np.cos(obs_dec*np.pi/180.))*
+         (-1.0*phi0*np.cos(obs_ang)-psi0*np.sin(obs_ang))/3600.)
+  dec0 = obs_dec - (-1.0*phi0*np.sin(obs_ang)+psi0*np.cos(obs_ang))/3600.
+
+  #--Convert to xmc coordinates--
+  
+  #-distance from input coordinates to center-
+  phi_rot =  -(ra - ra0)*np.cos(obs_dec*np.pi/180.)*3600. 
+  psi_rot = (dec - dec0)*3600.
+
+  #-rotated phi and psi-
+  phi = phi_rot*np.cos(obs_ang)+psi_rot*np.sin(obs_ang)
+  psi = -phi_rot*np.sin(obs_ang)+psi_rot*np.cos(obs_ang)
+
+  #-shift according to xmc center coords-
+  phi = phi + phi0
+  psi = psi0 - psi
+
+  return [phi,psi]
 #----------------------------------------------------------
