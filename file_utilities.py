c176bcd0eddbe6b5acad9bf3e13e209c86d06da5
Kari Frank <kaf33@E052497.astro.psu.edu>
2015-12-14 17:40:25 -0500

    Reformatted docstrings in file_utilities.py

--- file_utilities.py
+++ file_utilities.py
@@ -1,38 +1,41 @@
-#----------------------------------------------------------
-#Module of miscellaneous useful functions involving files
-#
-#Contains the following functions:
-#
-# ls_to_list
-# fetch_file
-# read_list
-# file_lines
-# parse_file_line
-#----------------------------------------------------------
+"""
+file_utilities.py
 
+Module of miscellaneous low-level useful functions involving files.
+
+Contains the following functions:
+
+ ls_to_list
+ fetch_file
+ read_list
+ file_lines
+ parse_file_line
+"""
+
+#----------------------------------------------------------
 #-import common modules-
 import re,sys,commands
 import os
 
 #----------------------------------------------------------
-#ls_to_list()
-
-#Author: Kari Frank
-#Date: January 22, 2013
-#Purpose: read in a path and return list of all files in that 
-#         directory as list of strings
-#Usage: ls_to_list('search_dir')
-#Input: 
-#  search_dir: string containing the full path to desired directory
-#  ls_args: optional string containing arguments to the ls command
-#Output:
-#  returns a list of strings, one element per file
-#Usage Notes:
-# 
-# 
-
 def ls_to_list(search_dir,ls_args=''):
+  """
+  Author: Kari Frank
+  Date: January 22, 2013
 
+  Purpose: read in a path and return list of all files in that 
+           directory as list of strings
+
+  Input: 
+    search_dir (str): full path to desired directory
+    ls_args (optional str): arguments to the ls command
+
+  Output:
+    returns a list of strings, one element per file
+
+  Usage Notes:
+
+  """ 
   #-set initial directory-
   pwd = os.getcwd()
     
@@ -52,36 +55,36 @@ def ls_to_list(search_dir,ls_args=''):
 
 
 #----------------------------------------------------------
-#fetch_file()
+def fetch_file(search_dir,pat='evt2',prompt=True):
+  """
+  Author: Kari Frank
+  Date: January 22, 2013
 
-#Author: Kari Frank
-#Date: January 22, 2013
-#Purpose: get a single file from given directory that 
-#         matches the search string pattern. if multiple 
-#         matching files or none, can prompt user for
-#         file path.
-#Usage: fetch_file('search_dir',pat)
-#Input: 
-#  search_dir: string containing the full path to directory
-#  pat: regular expression string containing the desired
-#        pattern to match (e.g. 'evt2'). default='evt2'
-#  prompt: Only matters if multiple matching files are found.
-#          if set to 'no' then will not prompt user for file
-#          in event of multiple matching files, will instead
-#          just pick the first one and print out a warning.
-#          can also be set to a (string)number, in which case it 
-#          will automatically choose that numbered file from the 
-#          available choices (starts with 0), as if the user
-#          had typed a number with prompt='yes'. 
-#          default is prompt='yes'. 
-#
-#Output:
-#  string containing full path to a single file
-#
-#Usage Notes:
-# - If there is no file that matches, will result in an error.
-#
-def fetch_file(search_dir,pat='evt2',prompt='yes'):
+  Purpose: get a single file from given directory that 
+           matches the search string pattern. if multiple 
+           matching files or none, can prompt user for
+           file path.
+
+  Input: 
+    search_dir (str): full path to directory
+    pat (str): regular expression containing the desired
+          pattern to match (e.g. 'evt2'). default='evt2'
+    prompt (bool): Only matters if multiple matching files are found.
+            if set to False then will not prompt user for file
+            in event of multiple matching files, will instead
+            just pick the first one and print out a warning.
+            can also be set to a (string)number, in which case it 
+            will automatically choose that numbered file from the 
+            available choices (starts with 0), as if the user
+            had typed a number with prompt=True. 
+            default is prompt=True. 
+
+  Output:
+    string containing full path to a single file
+
+  Usage Notes:
+   - If there is no file that matches, will result in an error.
+  """
 
   #-create list of all files in the directory
   # which match the given pattern- 
@@ -96,12 +99,12 @@ def fetch_file(search_dir,pat='evt2',prompt='yes'):
   if len(good_files) == 1: #if only one match found, return the path
     return good_files[0] 
   elif len(good_files) > 1: #if multiple matching files
-    if prompt == 'no':
+    if prompt is False:
       print 'Warning: Multiple files found matching '+pat+' ;'
       print '         choosing '+good_files[0]
       return good_files[0]
     else:
-      if prompt == 'yes': #if prompt was not set to a number
+      if prompt is True: #if prompt was not set to a number
         print 'More than one file found matching '+pat+','
         print 'please choose from the following:'
         for g in range(len(good_files)):
@@ -111,7 +114,7 @@ def fetch_file(search_dir,pat='evt2',prompt='yes'):
         ind = int(prompt)
       return good_files[ind]
   else: #if no matching files found
-    if prompt=='no':
+    if prompt is False:
       sys.exit('ERROR: no file found in '+search_dir+' matching '+pat)
     else:
       inquiry = 'No file found matching '+pat+'\nEnter full path to file: '
@@ -121,32 +124,28 @@ def fetch_file(search_dir,pat='evt2',prompt='yes'):
       else:
         return file
 #----------------------------------------------------------
-
-#----------------------------------------------------------
-#read_list()
-
-#Author: Kari Frank
-#Date: July 30, 2013
-#Purpose: return a list containing one element for each line
-#         of the specified file
-#
-#Usage: list = read_list(listfile,comment=comment)
-#
-#Input: 
-#  listfile: string containing the path and name of the file
-#            to be read  
-#
-#  comment: optional string to define as comment character.
-#           lines beginning with comment will be ignored.
-#
-#Output:
-#  - Returns a list of the file contents (one element per line).
-#    New line characters are stripped from the end of lines.
-#
-#Usage Notes:
-#
-#
 def read_list(listfile,comment=''):
+  """
+  Author: Kari Frank
+  Date: July 30, 2013
+
+  Purpose: return a list containing one element for each line
+           of the specified file
+
+  Usage: list = read_list(listfile,comment=comment)
+
+  Input: 
+    listfile (str): path to the file to be read  
+    comment (optional str): comment character.
+             lines beginning with comment will be ignored.
+
+  Output:
+    - Returns a list of the file contents (one element per line).
+      New line characters are stripped from the end of lines.
+
+  Usage Notes:
+
+  """
 
   #-open file-
   thefile = open(listfile)
@@ -172,28 +171,24 @@ def read_list(listfile,comment=''):
   return good_lines
 
 #----------------------------------------------------------
-#file_lines()
-
-#Author: Kari Frank
-#Date: March 20, 2014
-#Purpose: return the number of non-commented lines in a file
-#
-#Usage: nlines = file_lines(infile,comment=comment)
-#
-#Input: 
-#  infile: string containing the path and name of the file
-#            to be read  
-#
-#  comment: optional string to define as comment character.
-#           lines beginning with comment will be ignored.
-#
-#Output:
-#  - Returns number of non-commented lines in file 
-#
-#Usage Notes:
-#
-#
 def file_lines(infile,comment=''):
+  """
+  Author: Kari Frank
+  Date: March 20, 2014
+
+  Purpose: return the number of non-commented lines in a file
+
+  Input: 
+    infile (str): path and name of the file to be read  
+    comment (optional str): comment character.
+             lines beginning with comment will be ignored.
+
+  Output:
+    - Returns number of non-commented lines in file 
+
+  Usage Notes:
+
+  """
 
   #--get list of lines--
 #  lines = read_lines(infile,comment=comment)
@@ -207,32 +202,27 @@ def file_lines(infile,comment=''):
   return counter
 
 #----------------------------------------------------------
-# parse_file_line()
-#
-#Author: Kari A. Frank 
-#Date: March 20, 2014
-#Purpose: Read a first line from a file and parse it into a string list.
-#          
-#
-#Usage: parnames = parse_file_line(infile,delimiter=delimiter)
-#
-#Input:
-#
-#  infile    -- Path to text file.
-#  delimiter -- Optional delimiter(s) (string).  Default is space,
-#                comma, tab, and semicolon.  Can provide more than
-#                one delimiter character in the string.  Spaces and 
-#                tabs should be given in regular expression syntax,
-#                \s and \t.
-#
-#Output:
-#  
-#  returns string list of 'words' in the line.
-#
-#Usage Notes:
-#
-
 def parse_file_line(infile,delimiter=',;\s\t'):
+  """
+  Author: Kari A. Frank 
+  Date: March 20, 2014
+
+  Purpose: Read a first line from a file and parse it into a string list.
+
+  Input:
+    infile (str): Path to text file.
+    delimiter (optional str): delimiter(s).  Default is space,
+                  comma, tab, and semicolon.  Can provide more than
+                  one delimiter character in the string.  Spaces and 
+                  tabs should be given in regular expression syntax,
+                  \s and \t.
+
+  Output:  
+    returns string list of 'words' in the line.
+
+  Usage Notes:
+
+  """
 
   #--set delimiter string--
   delstr = r'[^'+delimiter+']+'
@@ -255,3 +245,5 @@ def parse_file_line(infile,delimiter=',;\s\t'):
 
   #--return string list--
   return outlist
+
+#----------------------------------------------------------
