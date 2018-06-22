70db7857db1e55657c3ebfdf4074e5ae510f3584
Kari Frank <kafrank@dhcp-10-105-125-46.wireless.northwestern.private>
2018-06-22 11:15:31 -0500

    diagnostics.clean(): fixed em_to_mass2 bug

--- diagnostics.py
+++ diagnostics.py
@@ -97,8 +97,8 @@ def clean(runpath='./',itmin=0,itmax=None,distance=8.0):
         df['blob_numberdensity'] = astro.em_to_density(df['blob_em'],\
                                    df['blob_volume'],density_type='number')
 
-        df['blob_mass2'] =astro.em_to_mass2(df['blob_em'],df['blob_volume'],
-                                          tounit='sol')        
+        df['blob_mass2'] =astro.em_to_mass2(df['blob_em'],df['blob_sigma'],
+                                          tounit='sol',distance=distance)   
 
     # -- save as file --
     outfile = ('deconvolution_merged_iter'
